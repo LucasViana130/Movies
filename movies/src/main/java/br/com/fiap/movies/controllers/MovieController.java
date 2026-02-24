ac86d04b994b53865a5f38ea9fa4bc724339bd45
LucasViana130 <lucasgviana1@gmail.com>
2026-02-24 20:53:32 -0300

    add dependencies

--- movies/src/main/java/br/com/fiap/movies/controllers/MovieController.java
+++ movies/src/main/java/br/com/fiap/movies/controllers/MovieController.java
@@ -1,12 +1,14 @@
 package br.com.fiap.movies.controllers;
 
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class MovieController {
 
-    @RequestMapping(path = "/")
+    @GetMapping("/")
     public String healthCheck(){
         return "Server UP";
     }
