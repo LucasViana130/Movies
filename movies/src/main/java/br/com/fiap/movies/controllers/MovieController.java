cf26418cc54f71bf8b0a1cfedcf177c9118c676b
LucasViana130 <lucasgviana1@gmail.com>
2026-03-02 20:58:07 -0300

    Api testes

--- movies/src/main/java/br/com/fiap/movies/controllers/MovieController.java
+++ movies/src/main/java/br/com/fiap/movies/controllers/MovieController.java
@@ -1,17 +1,24 @@
 package br.com.fiap.movies.controllers;
 
+import br.com.fiap.movies.models.Movie;
+import br.com.fiap.movies.services.MovieService;
 import org.slf4j.ILoggerFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDate;
+import java.util.List;
 
 @RestController
 public class MovieController {
 
     private final Logger log = LoggerFactory.getLogger(getClass());
+
+    @Autowired
+    private MovieService service;
 
     @GetMapping("/")
     public String healthCheck(){
@@ -19,9 +26,16 @@ public class MovieController {
     }
 
     @GetMapping("/movies")
-    public Movie getMovies(){
+    public List<Movie> getMovies(){
         log.info("Listando todos os filmes");
-        return null;//mocky
+        return service.getMovies();
+    }
+
+    @PostMapping("/movies")
+    @ResponseStatus(code = HttpStatus.CREATED)
+    public Movie addMovie(@RequestBody Movie movie){
+        log.info("Cadastrando novo filme...");
+        return service.addMovie(movie);
     }
 
 }
--- movies/src/main/java/br/com/fiap/movies/models/Movie.java
+++ movies/src/main/java/br/com/fiap/movies/models/Movie.java
@@ -1,4 +1,16 @@
 package br.com.fiap.movies.models;
 
+import lombok.*;
+
+import java.time.LocalDate;
+import java.util.Objects;
+
+@Data
+@AllArgsConstructor
 public class Movie {
+    private String title;
+    private String synopsis;
+    private Integer rating;
+    private LocalDate releaseDate;
+
 }
